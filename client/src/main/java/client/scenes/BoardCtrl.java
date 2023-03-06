618b7e0261953766106b51240cff4ed3ecacec5e
Sagar Chethan Kumar <S.ChethanKumar@student.tudelft.nl>
2023-03-06 22:36:38 +0100

    checkstyle fix

--- client/src/main/java/client/scenes/BoardCtrl.java
+++ client/src/main/java/client/scenes/BoardCtrl.java
@@ -3,8 +3,6 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 
-import javax.swing.plaf.synth.SynthTextAreaUI;
-
 public class BoardCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
