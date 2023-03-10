a20e99e7807ea18c3b9d965688973cff46e18b7e
Teun den Boer <T.W.denboer@student.tudelft.nl>
2023-03-10 22:06:29 +0100

    pipeline fix (remove import)

--- server/src/main/java/server/database/CardRepository.java
+++ server/src/main/java/server/database/CardRepository.java
@@ -1,7 +1,6 @@
 package server.database;
 
 import commons.Card;
-import commons.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, String> {}
