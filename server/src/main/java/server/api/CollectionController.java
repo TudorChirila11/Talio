9f4cb296380ef02e383bdf449809cb03214cc441
Teun den Boer <T.W.denboer@student.tudelft.nl>
2023-03-10 21:16:06 +0100

    Delete request implemmented

--- server/src/main/java/server/api/CollectionController.java
+++ server/src/main/java/server/api/CollectionController.java
@@ -1,29 +1,10 @@
-/*
- * Copyright 2021 Delft University of Technology
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package server.api;
 
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import commons.Collection;
 import server.database.CollectionRepository;
@@ -77,6 +58,20 @@ public class CollectionController {
 
         Collection saved = repo.save(collection);
         return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * this will delete the collection
+     * @param id the id / name of the collection
+     * @return the response entity
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") String id) {
+        if (!repo.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        repo.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
