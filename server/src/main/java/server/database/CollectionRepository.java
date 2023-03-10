412436b729aa568a334e0da4ea080241e08f0131
Teun den Boer <T.W.denboer@student.tudelft.nl>
2023-03-10 18:02:39 +0100

    Collection respository

--- server/src/main/java/server/database/CollectionRepository.java
+++ server/src/main/java/server/database/CollectionRepository.java
@@ -1,10 +1,8 @@
 package server.database;
 
-package server.database;
+
 
 import commons.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import commons.Quote;
 
 public interface CollectionRepository extends JpaRepository<Collection, String> {}
