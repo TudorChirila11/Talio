ada7ffa65024f057f11d06b3b89f7f976a46daa4
Oleh Grypas <o.grypas@student.tudelft.nl>
2023-03-12 15:57:43 +0100

    added the collection controller to the checkstyle standards

--- client/src/main/java/client/scenes/CollectionOverviewCtrl.java
+++ client/src/main/java/client/scenes/CollectionOverviewCtrl.java
@@ -20,21 +20,15 @@ import com.google.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class CollectionOverviewCtrl implements Initializable {
 
     @FXML
     private VBox container ;
-
-    private List<Label> labels ;
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
 
