9f8ba87cc9ba1872057d7dead5bf0880486a8aa8
Teun den Boer <T.W.denboer@student.tudelft.nl>
2023-03-17 20:26:06 +0100

    added delete board repo

--- server/src/main/java/server/api/ResetController.java
+++ server/src/main/java/server/api/ResetController.java
@@ -1,8 +1,10 @@
 package server.api;
 
+import commons.Board;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import server.database.BoardRepository;
 import server.database.CardRepository;
 import server.database.CollectionRepository;
 
@@ -12,15 +14,19 @@ public class ResetController {
 
     private final CollectionRepository repoCollection;
     private final CardRepository repoCard;
+    private final BoardRepository repoBoard;
 
     /**
      * Controller constructor
      * @param repoCollection repo reference
      * @param repoCard the repository of the cards
+     * @param repoBoard the repository of the boards
      */
-    public ResetController(CollectionRepository repoCollection, CardRepository repoCard) {
+    public ResetController(CollectionRepository repoCollection,
+                           CardRepository repoCard, BoardRepository repoBoard) {
         this.repoCollection = repoCollection;
         this.repoCard = repoCard;
+        this.repoBoard = repoBoard;
 
     }
 
@@ -32,6 +38,7 @@ public class ResetController {
     public ResponseEntity<Void> deleteAll() {
         repoCollection.deleteAll();
         repoCard.deleteAll();
+        repoBoard.deleteAll();
         return ResponseEntity.noContent().build();
     }
 }
