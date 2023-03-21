965bdee4d831d073205fb7c5a165ac5d3a39af9c
Sagar Chethan Kumar <S.ChethanKumar@student.tudelft.nl>
2023-03-21 18:11:46 +0100

    New Client Builder for ServerUtils

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -23,7 +23,11 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
 
+import commons.Board;
 import commons.Card;
+import commons.Collection;
+import javafx.scene.control.Alert;
+import javafx.stage.Modality;
 import org.glassfish.jersey.client.ClientConfig;
 
 import commons.Quote;
@@ -76,9 +80,9 @@ public class ServerUtils {
 
 
     /**
-     * Adding a new quote to the server DB
-     * @param card a Quote object
-     * @return returns a Quote
+     * Adding a new Card to the server DB
+     * @param card a Card object
+     * @return returns a Card
      */
     public Card addCard(Card card) {
         return ClientBuilder.newClient(new ClientConfig()) //
@@ -86,5 +90,105 @@ public class ServerUtils {
                 .request(APPLICATION_JSON) //
                 .accept(APPLICATION_JSON) //
                 .post(Entity.entity(card, APPLICATION_JSON), Card.class);
+    }
+
+    /**
+     * Adds a card to collection
+     * @param card card
+     * @param collection collection
+     * @return a collection.
+     */
+    public Collection addCardCollection(Card card, Collection collection){
+        return ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/collections/CardAddTo/" + card.getId() + "/" +card.getCollectionId()) //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON) //
+                .post(Entity.entity(collection, APPLICATION_JSON), Collection.class);
+    }
+
+    /**
+     * Retrieves all Cards
+     * @return List of cards
+     */
+    public List<Card> getCards() {
+        return ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/cards") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON) //
+                .get(new GenericType<List<Card>>() {});
+    }
+
+    /**
+     * Adding a new quote to the server DB
+     * @param collection a Quote object
+     * @return returns a Quote
+     */
+    public Collection addCollection(Collection collection) {
+        return ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/collections") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON) //
+                .post(Entity.entity(collection, APPLICATION_JSON), Collection.class);
+    }
+
+    /**
+     * Retrieves all Collections
+     * @return List of collections
+     */
+    public List<Collection> getCollections() {
+        return ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/collections") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON) //
+                .get(new GenericType<List<Collection>>() {});
+    }
+
+    /**
+     * Retrieves all Collections
+     */
+    public void resetState() {
+        ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/collections") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON)
+                .delete();
+
+        ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/boards") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON)
+                .delete();
+
+        ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/cards") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON)
+                .delete();
+    }
+
+    /**
+     * Adds a board to the Server DB
+     * @param board type
+     * @return board the board added
+     */
+    public Board addBoard(Board board){
+        return ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/boards") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON) //
+                .post(Entity.entity(board, APPLICATION_JSON), Board.class);
+    }
+
+    /**
+     * Retrieves the only board
+     * @return a board
+     */
+    public Board getBoard(){
+        return ClientBuilder.newClient(new ClientConfig()) //
+                .target(SERVER).path("api/boards") //
+                .request(APPLICATION_JSON) //
+                .accept(APPLICATION_JSON) //
+                .get(new GenericType<List<Board>>() {}).get(0);
+
     }
 }
