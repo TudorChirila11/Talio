a7db4c961b28f7761ab4f440f8c3b66134ce0886
Oleh Grypas <o.grypas@student.tudelft.nl>
2023-03-27 19:10:40 +0200

    Another checkstyle error I missed

--- server/src/main/java/server/api/BoardController.java
+++ server/src/main/java/server/api/BoardController.java
@@ -1,7 +1,6 @@
 package server.api;
 
 import commons.Board;
-import commons.Collection;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
