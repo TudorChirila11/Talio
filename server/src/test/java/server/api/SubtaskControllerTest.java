69c474eac79534a18fb665468eeaf52bcb706a7f
Tudor Chirila <t.chirila@student.tudelft.nl>
2023-04-01 16:51:01 +0200

    checkstyle fix

--- server/src/test/java/server/api/SubtaskControllerTest.java
+++ server/src/test/java/server/api/SubtaskControllerTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.*;
 
+//CHECKSTYLE:OFF
 class SubtaskControllerTest {
 
     private TestSubtaskRepository repo;
